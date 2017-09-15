b213d478d5a4c6147a231c9fb541acf0ea85c566
Alexandr <alxmalafeev@gmail.com>
2017-09-15 17:17:17 +0300

    add object

--- canvas.js
+++ canvas.js
@@ -4,38 +4,48 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var x = 300;
-var y = 300;
-var VelocityMultX = 10;
-var VelocityMultY = 10;
-var radius = 30;
+// var x = 300;
+// var y = 300;
+// var velocityMultX = 10;
+// var velocityMultY = 10;
+// var radius = 50;
+// var randomPositionX = getRandomNumber(radius, innerWidth);
+// var randomPositionY = getRandomNumber(radius, innerHeight);
+// var dx = velocityMultX * getRandomBoolean();
+// var dy = velocityMultY * getRandomBoolean();
+var strokeColor = "#fe34a5";
 
-var randomPositionX = getRandomNumber(radius, innerWidth);
-var randomPositionY = getRandomNumber(radius, innerHeight);
-var randomVelocityX = VelocityMultX * getRandomBoolean();
-var randomVelocityY = VelocityMultY * getRandomBoolean();
+function Circle(x, y, dx, dy, radius) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
 
-
-(function animate() {
-	requestAnimationFrame(animate);
-	c.clearRect(0, 0, innerWidth, innerHeight);
-
-	c.beginPath();
-	c.arc(randomPositionX, randomPositionY, radius, 0, Math.PI * 2, false);
-	c.strokeStyle = "#fe34a5";
-	c.stroke();
-
-	if (randomPositionX + radius > innerWidth || randomPositionX - radius < 0) {
-		randomVelocityX = -randomVelocityX;
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = strokeColor;
+		c.stroke();
 	}
 
-	 if (randomPositionY + radius > innerHeight || randomPositionY - radius < 0) {
-	 	randomVelocityY = -randomVelocityY;
-	 }
+	this.animate = function() {
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+			this.dx = -this.dx;
+		}
 
-	randomPositionX += randomVelocityX;
-	randomPositionY += randomVelocityY;
-})();
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+			this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+
+		this.draw();
+	}
+}
+
+var circle = new Circle(200, 200, 2, 2, 30);
 
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -44,3 +54,9 @@ function getRandomNumber(min, max) {
 function getRandomBoolean() {
 	return Math.random() - 0.5;
 }
+
+(function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+	circle.animate();
+})();
